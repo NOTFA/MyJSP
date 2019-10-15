f1c29d219fe30f686dcb1154a40657422a92d320
305 <305@305-PC>
2019-10-15 14:29:12 +0900

    20191015 JSP 오후

--- MyJSP/WebContent/member.jsp
+++ MyJSP/WebContent/member.jsp
@@ -2,6 +2,9 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
 <%@ page import="java.util.HashMap"%>
 <%@ page import="java.util.Set"%>
+<%@ page import="java.util.List"%>
+<%@ page import="com.kb.org.member.MemberVO"%>
+
 <!DOCTYPE>
 <html>
 <head>
@@ -15,15 +18,32 @@
 		<!-- 중간 -->
 		<div class="row">
 			<div class="col">
+			<table class="table">
+				<tr>
+					<td>순번</td>
+					<td>아이디</td>
+					<td>비밀번호</td>
+					<td>이름</td>
+					<td>성별</td>
+					<td>가입날짜</td>
+				</tr>
 				<%
-					HashMap<String,String> hm = (HashMap)request.getAttribute("hm");
-					Set<String> keys = hm.keySet();
-					for(String key : keys)
-						out.println(key + hm.get(key) + "<br/>");
+ // 					List<MemberVO> list = (List) request.getAttribute("MyList"); -->
+ // 					for( MemberVO vo :list) -->
+ // 						out.println(vo.getName()); -->
 				%>
-				<c:forEach items="${hm}" var="i">
-					${i}<br/>
+				
+				<c:forEach items="${myList}" var="i">
+					<tr>
+					<td>${i.seq}</td>
+					<td>${i.id}</td>
+					<td>${i.pwd}</td>
+					<td>${i.name}</td>
+					<td>${i.gender}</td>
+					<td>${i.joindate}</td>
+					</tr>
 				</c:forEach>
+			</table>
 			</div>
 		</div>
 		<!-- 발 -->
--- MyJSP/src/com/kb/org/MainController.java
+++ MyJSP/src/com/kb/org/MainController.java
@@ -1,7 +1,11 @@
 package com.kb.org;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +13,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kb.org.member.MemberVO;
 
 @WebServlet("*.do")
 public class MainController extends HttpServlet {
@@ -19,7 +25,7 @@ public class MainController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = null;
+		RequestDispatcher rd = null; // jsp파일지정
 		request.setCharacterEncoding("UTF-8");
 		String reqURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
@@ -37,7 +43,33 @@ public class MainController extends HttpServlet {
 			rd = request.getRequestDispatcher("index.jsp");
 		}
 		else if(cmd.equals("/member.do")) {
-	
+			
+			try {
+				List<MemberVO> list = new ArrayList<>();
+				Connection conn = ConnectionPool.getConnection(); // context.xml 에 DB연결
+				PreparedStatement pstmt = conn.prepareStatement("select * from member");
+				ResultSet rs = pstmt.executeQuery();
+				
+				while (rs.next()) {
+					list.add(
+						new MemberVO(
+							rs.getString("name"),
+							rs.getString("id"),
+							rs.getString("pwd"),
+							rs.getInt("seq"),
+							rs.getString("joindate"),
+							rs.getString("gender")
+						)		
+					);
+				}
+				
+				request.setAttribute("myList",list);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			rd = request.getRequestDispatcher("member.jsp");
 		}
 		else if(cmd.equals("/freeboard.do")) {
 			rd = request.getRequestDispatcher("freeboard.jsp");
